7b18f5e6e9604ffbe448c0fa704f30fe2436f0e7
Minion <34243578+Minion3665@users.noreply.github.com>
2020-05-21 21:27:20 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MiniUtils',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/Minion3665/MiniUtils',
+    license='GNU General Public License V3',
+    author='Minion3665',
+    author_email='nathan@clicksminuteper.net',
+    description='The MiniUtils module to make making discord bots easier'
+)
